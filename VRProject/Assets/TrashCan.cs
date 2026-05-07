5ea0453e23e0d56ea4a7c71e47482e2260c8a618
KirisameSeanight <1250612230@qq.com>
2026-05-08 04:20:43 +0800

    upload new Unity project

---
+++ VRProject/Assets/TrashCan.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<TriggerZone>().OnEnterEvent.AddListener(InsideTrash);
+    }
+    public void InsideTrash(GameObject go)
+    {
+        go.SetActive(false); 
+    }
+}
